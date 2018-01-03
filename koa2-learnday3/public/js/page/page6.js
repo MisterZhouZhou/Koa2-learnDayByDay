5c19d789ed0bd4871d30cf4fe8788d7aa4794690
MisterZhouZhou <m15038960016@163.com>
2018-01-03 19:58:09 +0800

    update

--- koa2-learnday3/public/js/page/page6.js
+++ koa2-learnday3/public/js/page/page6.js
@@ -1,6 +1,6 @@
 var app = angular.module('myApp',[]);
 app.controller('siteCtrl', ($scope,$http)=>{
-  $http.get('http://127.0.0.1:3000/sites')
+  $http.get('/sites')
   .then((response)=>{
      $scope.names = response.data.sites;
   });
