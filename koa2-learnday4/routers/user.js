6cd7cd8ab6a88abe60425eaa3eed0c7953dea251
MisterZhouZhou <m15038960016@163.com>
2018-01-05 12:04:24 +0800

    update

--- koa2-learnday4/routers/user.js
+++
@@ -1,8 +0,0 @@
-const router = require('koa-router')();
-const UserController = require('../controllers/user');
-
-router.post('/login', UserController.login);
-
-router.post('/register', UserController.register);
-
-module.exports  = router;
